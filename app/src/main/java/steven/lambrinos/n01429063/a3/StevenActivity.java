0bf320b5f9f9bc53aff5b9100ffe5f43bae06c0b
stevenlambrinos <Stevielam442@gmail.com>
2022-07-06 00:09:19 -0400

    Initial Commit
    Created and edited the files to the proper spec needed for the assignment.

--- app/src/main/java/steven/lambrinos/n01429063/a3/StevenActivity.java
+++ app/src/main/java/steven/lambrinos/n01429063/a3/StevenActivity.java
@@ -1,14 +1,14 @@
+//Steven Lambrinos N01429063 RNA
 package steven.lambrinos.n01429063.a3;
-
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import steven.lambrinos.n01429063.a3.databinding.ActivityMainBinding;
 
@@ -19,15 +19,11 @@ public class StevenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_notifications, R.id.navigation_dashboard)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
